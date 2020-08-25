68c4eaa40e9ef1fe5efc1b70cabba6b78dfbe8c9
MeneerAB <69039273+MeneerAB@users.noreply.github.com>
2020-08-25 15:38:51 +0200

    Update bot.js
    Added ~shutdown(admin only) and ~info commands and fixed the ~help command

--- bot.js
+++ bot.js
@@ -9,32 +9,58 @@ bot.on('message', message => {
 
     //Variables
     var sender = message.author; // Msg sender
+    
+// Calling the package
+var Discord = require("discord.js");
+var bot = new Discord.Client();
+var client = new Discord.Client();
+
+
+
+// Listener Event: Message Received (Runs every msg)
+bot.on('message', message => {
+
+    
+
+    //Variables
+    var sender = message.author; // Msg sender
     var msg = message.content.toLowerCase(); //  Msg uppercase
     var prefix = "~" // The text before commands
     
 
+    if (!message.content.startsWith(prefix)){
+         return;
+    }
 
     if(sender.id === '732619827629522974') {
         return; 
+    }
+
+    if (msg === prefix + "shutdown") {
+        if (message.author.id === "400734569495658507") {
+                console.log('But is shutting down')
+                message.channel.send('Bot is shutting down')
+                process.exit();
+            
+        }   
+        else { 
+            message.channel.send('You arent my owner, dont play games')
+            console.log(`${message.author.username} (${message.author.id}) tried to shut the bot down at ${new Date()}`)
+            return; 
+        }
     }
 
     //Ping / Pong command
     if (msg === prefix + 'ping') {
         message.channel.send({embed:{
             title:'Ping',
-            description: `Pong!: ${bot.ping} ms`,
+            description: `Pong!: ${Math.round(bot.ws.ping)} ms`,
             color: 0xA0412D,
             timestamp: new Date(),
             footer: { 
              text: `Requested by ${message.author.username} `
             }
-        }})
-
-
-        
-
-
-}
+        }})}
 
         if (msg === prefix + 'invite') {
             message.channel.send({embed:{
@@ -48,64 +74,94 @@ bot.on('message', message => {
             }})
             }
             
-            if (msg === prefix + 'spacecool?')
-                message.channel.send('he is super cool and the best boy')
-
-    if (message.channel.id === 'id here') {
-        if (isNaN(message.content)) {
-            message.delete() 
-            message.author.send("Only numbers you idiot")
+        if (msg === prefix + 'spacecool?') { 
+            message.channel.send({embed:{
+                title: 'Yes',
+                color: 0xA0412D,
+                image: {
+                    url: 'https://preview.redd.it/balstg2lsy651.jpg?width=960&crop=smart&auto=webp&s=8348f36f2a51cddedf423673d3805ad7b3d789f3'
+                }
+               
+            }})
+                
+            
         }
-    }   
+
+        if (msg === prefix + "spacenotcool?") {
+            message.channel.send(`${message.author} go f*ck yourself`)
+        }
     
-    if (msg === prefix + "howdoistream?")
-    message.channel.send({embed:{
-        title:'How do I stream?',
-        color: 0xA0412D,
-        fields:[
-            {
-                name: "You read the wiki",
-                value: "[this one](https://www.reddit.com/r/pan/wiki/index)"
+   
+
+
+    if (msg === prefix + "howdoistream?") 
+        message.channel.send({embed:{
+            title:'How do I stream?',
+            color: 0xA0412D,
+            fields:[
+                {
+                   name: "You read the wiki",
+                  value: "[this one](https://www.reddit.com/r/pan/wiki/index)"
+              }
+            ],
+            timestamp: new Date(),
+            footer: { 
+             text: `Requested by ${message.author.username} `
             }
-        ],
-        timestamp: new Date(),
-        footer: { 
-         text: `Requested by ${message.author.username} `
-        }
     }})
 
-    if (msg === prefix + "help" || "commands" )
-    message.channel.send({embed:{
-        title:'List of my commands',
-        color: 0xA0412D,
-        fields:[
-            {
-                name: "`Help/Commands`",
-                value: "Gives this list"
-            },
-            {
-                name: "`ping`",
-                value: "Gives my ping",
-            },
-            {
-                name: "`invite`",
-                value: "gives my invite link"
-            },
+   if (msg === prefix + 'help' || (msg === prefix + 'commands')) {
+        message.channel.send({embed:{
+            title: 'List of my commands',
+            color: 0xA0412D,
+            fields:[
+                {
+                    name: '`help/commands` - gives this list '
+                },
+                {
+                    name: '`spacecool?` - tells you the truth'
+                },
+                {
+                    name: '`spacenotcool?` - tells you the truth again' 
+                },
+                {
+                    name: '`howdoistream?` - tells you how to stream on rpan '
+                },
+                {
+                    name: '`ping` - tells you my ping '
+                },
+                {
+                    name: '`invite` - sends you my invite link ', 
+                },
+                {
+                    name: '`info` - gives you info about the bot '
+                }
 
-            { 
-                name: "`spacecool?`",
-                value: "Tells you the truth"
-            },
-            { 
-                name: "`howdoistream?`",
-                value: "Gives you the link to rpans wiki"
-            },
-        ],
-        timestamp: new Date(),
-        footer: { 
-         text: `Requested by ${message.author.username} `
-        }
-    }})
+            ],
+            timestamp: new Date(),
+            footer: {
+                text: `requested by ${message.author.username}`
+            }
+
+        
+        }})
+   }
+
+   if (msg === prefix + 'info')
+        message.channel.send({embed:{
+            title: 'Info about me',
+            color: 0xA0412D,
+            fields: [
+                {
+                    name: 'My developer is MeneerAB',
+                    value: '[My github] (https://github.com/MeneerAB/bot/blob/master/bot.js) '
+                }
+            ],
+            timestamp: new Date(),
+            footer: {
+                text: `requested by ${message.author.username}`
+            }
+        }})
 });
 
 //Launch bot
@@ -115,10 +171,10 @@ bot.on('ready', () => {
     bot.user.setStatus("dnd") // Online, idle, invisible or dnd
     
     bot.user.setActivity("~help")
-
+    bot.user.setActivity('My developers screams', { type: 'LISTENING' });
 
 });
 
 
 // Login
-bot.login("howboutno") 
+bot.login("not telling ya") 
